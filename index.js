6411060095d61217363bb1393f2c6ce4e7f50775
gregoryjohncarter <gregoryjohncarter@gmail.com>
2022-01-20 13:38:45 -0500

    test readme Lernantino

--- index.js
+++ index.js
@@ -160,7 +160,7 @@ const promptUser = (questions) => {
 // function writeToFile(fileName, data) {
     const writeFile = fileContent => {
         return new Promise((resolve, reject) => {
-          fs.writeFile('./dist/README.md', JSON.stringify(fileContent, null, '\t'), err => {
+          fs.writeFile('./dist/README.md', JSON.stringify(fileContent).replace('"', '').replace(/\\n/g, ''), err => {
             // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
             if (err) {
               reject(err);
